2dcabf2c85dbbcd9f8a0f297b8ba254acd503cda
Dimas Leopoldo Mérida Flores <48163017+leomerida15@users.noreply.github.com>
2021-09-06 15:10:54 -0400

    respOk token

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,9 @@
 import { Application } from 'express';
 
 // rputers
-import auth from './auth/auth.routes';
 import client from './client';
 
 //
 export default (app: Application) => {
-	app.use('/auth', auth);
 	client(app);
 };
