9c178cec6b2d4d4194e4870b73d08b018d345208
root <root@DESKTOP-TUHIIIT.localdomain>
2021-10-29 00:57:06 -0400

    socket en espera

--- src/services/socket/router/web/index.ts
+++ src/services/socket/router/web/index.ts
@@ -1,7 +1,7 @@
 import { Server } from 'socket.io';
 
 const socket = (WSs: any) => {
-	const io = new Server(WSs);
+	const io = new Server(WSs, { cors: { origin: '*', methods: ['GET', 'POST'] } });
 
 	io.on('connection', (ws: any): void => {
 		console.log('a user connected');
