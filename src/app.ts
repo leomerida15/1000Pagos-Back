d5ce98c499fd9de6dc5664e79ce5f93abb85dcb0
Dimas Leopoldo Mérida Flores <48163017+leomerida15@users.noreply.github.com>
2021-11-02 09:52:24 -0400

    test coneccion a mssql

--- src/app.ts
+++ src/app.ts
@@ -11,18 +11,19 @@ createConnection()
 	.then(async () => {
 		await contents();
 
-		const app: Application = services.find((service: any): boolean => {
+		const sv = services.find((service: any): boolean => {
 			const keySer: string = service.key;
 
 			if (!process.env.npm_lifecycle_event) return false;
 			const key = process.env.npm_lifecycle_event.replace(/(serve:|start:)/i, '');
-			console.log('key', key);
 
 			return keySer === key;
-		}).app;
+		});
+
+		const { app, key } = sv;
 
 		app.listen(app.get('port'), () => {
-			console.log(`app corriendo en el puerto http://localhost:${app.get('port')} leoM   `);
+			console.log(`${key} corriendo en el puerto http://localhost:${app.get('port')} leoM   `);
 			console.log('_________');
 			console.log('|       |');
 			console.log('| ()_() |');
