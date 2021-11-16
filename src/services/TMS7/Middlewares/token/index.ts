e1629bb8559e539388e7ed8c290cb066a0abb722
Dimas Leopoldo Mérida Flores <48163017+leomerida15@users.noreply.github.com>
2021-11-16 14:49:58 -0400

    convert test

--- src/services/TMS7/Middlewares/token/index.ts
+++ src/services/TMS7/Middlewares/token/index.ts
@@ -9,25 +9,19 @@ export default (req: Request, res: Response, next: NextFunction) => {
 	try {
 		// define array route
 
-		// valid use
-		const result: boolean =
-			list.includes(req.baseUrl) || list.includes(req.path.split('/')[1]) || list.includes(req.path.split('/')[2]);
-
 		// use
-		if (result) {
-			if (req.headers.token) {
-				const { token }: any = req.headers;
-				const resp: any = jwt.verify(token, Key);
+		if (req.headers.token) {
+			const { token }: any = req.headers;
+			const resp: any = jwt.verify(token, Key);
 
-				console.log(resp);
+			console.log(resp);
 
-				req.headers.token = resp;
+			req.headers.token = resp;
 
-				next();
-				//
-			} else throw { status: false, message: 'JWT es requerido', code: 400 };
-		} else {
 			next();
+			//
+		} else {
+			throw { status: false, message: 'JWT es requerido', code: 400 };
 		}
 	} catch (err: any) {
 		err.code = 403;
--- src/services/TMS7/controllers/TMS7.ts
+++ src/services/TMS7/controllers/TMS7.ts
@@ -3,6 +3,8 @@ import { Api } from '../../../interfaces';
 import axios from 'axios';
 let token: string = '';
 
+let users: any[] = [];
+
 // const axios = Axios.create({
 // 	baseURL: 'http://10.198.72.86',
 // 	timeout: 1000,
@@ -40,7 +42,47 @@ export const Login = async (
 			},
 		});
 
-		token = resp.data.access_token;
+		const { token }: any = req.headers;
+		const { access_token } = resp.data;
+
+		const i = users.findIndex((user) => user.id === token.id);
+
+		if (i >= 0) users[i].access_token = resp.data.access_token;
+		else users.push({ ...token, access_token, body: req.body });
+
+		res.status(200).json({ message: 'Auth OK', info: { id: 1 } });
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const refreshAuth = async (
+	req: Request<any, Api.Resp, tms7Auth>,
+	res: Response,
+	next: NextFunction
+): Promise<void> => {
+	try {
+		const { token }: any = req.headers;
+
+		const i = users.findIndex((user) => user.id === token.id);
+
+		const bodyTMS7 = new URLSearchParams();
+
+		Object.keys(req.body).forEach((key: any) => bodyTMS7.append(key, body[key]));
+
+		const { body }: any = req;
+
+		req.body.grant_type = 'password';
+
+		Object.keys(req.body).forEach((key: any) => bodyTMS7.append(key, body[key]));
+
+		const resp = await axios.post('http://10.198.72.86/auth/token', bodyTMS7, {
+			headers: {
+				Connection: 'keep-alive',
+				'Accept-Encoding': 'gzip, deflate, br',
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		});
 
 		res.status(200).json({ message: 'Auth OK', info: { id: 1 } });
 	} catch (err) {
@@ -62,7 +104,7 @@ export const getAllCommerce = async (
 
 		Object.keys(req.body).forEach((key: any) => bodyTMS7.append(key, body[key]));
 
-		const resp = await axios.post('http://10.198.72.86/auth/token', bodyTMS7, {
+		const resp = await axios.post('http://10.198.72.86/TMS7API/v1/Merchant?net_id=0002', bodyTMS7, {
 			headers: {
 				Connection: 'keep-alive',
 				'Accept-Encoding': 'gzip, deflate, br',
