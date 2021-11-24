638289252a48ac0d87f7aae31fb13c0c8732dd6a
Dimas Leopoldo Mérida Flores <48163017+leomerida15@users.noreply.github.com>
2021-11-24 12:59:46 -0400

    arreglo en files para acta

--- src/services/files/controllers/1000pagos.controllers.ts
+++ src/services/files/controllers/1000pagos.controllers.ts
@@ -58,9 +58,9 @@ export const upFilesRecaudos = async (
 				'rc_comp_dep',
 			],
 		});
-		if (!fm) throw { message: 'no existe fm con esos datos', code: 400 };
+		// if (!fm) throw { message: 'no existe fm con esos datos', code: 400 };
 
-		if (fm.id_client && fm) {
+		if (fm?.id_client && fm) {
 			const { id_commerce, id_client } = fm;
 			const { rc_ident_card }: any = id_client;
 			const { rc_special_contributor, rc_constitutive_act, rc_ref_bank, rc_rif }: any = id_commerce;
