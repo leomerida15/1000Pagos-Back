9a6ec18e3533f93b7d4bf29022188c9b6fc7f280
Aldrin K. Mendoza Fernandez <amendoza@tranred.com.ve>
2021-11-28 21:06:57 -0400

    Aldrin Front-nue 28/11

--- src/services/socket/router/administracion.routes.ts
+++ src/services/socket/router/administracion.routes.ts
@@ -1,8 +1,14 @@
-import { administracion } from '../controllers/administracion';
+import { administracion, listAdminisWorking } from '../controllers/administracion';
 
 const administra = (io: any) => {
 	io.on('connection', (socket: any) => {
 		socket.emit('PruebaAdmin', administracion);
+
+		//Devuelve
+		socket.on('cliente:administrWorking', async (user: any, id: any) => {
+			await listAdminisWorking(socket.id, user, id);
+			io.emit('server:loadAdministra', administracion);
+		});
 	});
 };
 
