e979a57448a2b7bbdb7f6fbafdb0269bdfe66ee5
Dimas Leopoldo Mérida Flores <48163017+leomerida15@users.noreply.github.com>
2021-12-06 11:06:11 -0400

    files

--- src/app.ts
+++ src/app.ts
@@ -29,53 +29,5 @@ createConnection()
 			console.log('| DB OK |');
 			console.log('|_______|');
 		});
-
-		// let query: any = await getRepository(fm_request).findOne({
-		// 	where: { id: 2 },
-		// 	order: { id: 'ASC' },
-		// 	relations: [
-		// 		// client
-		// 		'id_client',
-		// 		'id_client.id_location',
-		// 		'id_client.id_location.id_estado',
-		// 		'id_client.id_location.id_municipio',
-		// 		'id_client.id_location.id_ciudad',
-		// 		'id_client.id_location.id_parroquia',
-		// 		'id_client.id_ident_type',
-		// 		// dir_pos
-		// 		'dir_pos',
-		// 		'dir_pos.id_location',
-		// 		'dir_pos.id_location.id_estado',
-		// 		'dir_pos.id_location.id_municipio',
-		// 		'dir_pos.id_location.id_ciudad',
-		// 		'dir_pos.id_location.id_parroquia',
-		// 		// commerce
-		// 		'id_commerce',
-		// 		'id_commerce.id_ident_type',
-		// 		'id_commerce.id_activity',
-		// 		'id_commerce.id_location',
-		// 		'id_commerce.id_location.id_estado',
-		// 		'id_commerce.id_location.id_municipio',
-		// 		'id_commerce.id_location.id_ciudad',
-		// 		'id_commerce.id_location.id_parroquia',
-		// 		'id_commerce.banks',
-		// 		'id_commerce.id_aci',
-		// 	],
-		// });
-		// if (query) {
-		// 	const dir_pos = await getRepository(fm_dir_pos).find({
-		// 		where: { id_request: query.id },
-		// 		relations: [
-		// 			'id_location',
-		// 			'id_location.id_estado',
-		// 			'id_location.id_municipio',
-		// 			'id_location.id_ciudad',
-		// 			'id_location.id_parroquia',
-		// 		],
-		// 	});
-		// 	query.dir_pos = dir_pos;
-		// 	console.log('dir_pos', query.dir_pos);
-		// }
-		// console.log('fmData', query);
 	})
 	.catch((err) => console.log('DB ERR', err));
