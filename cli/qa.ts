768d8fe6a5ca30978c1c2ef77d2b3789f08f5620
Dimas L. Merida Flores <dmerida@tranred.com.ve>
2021-12-07 17:44:41 -0400

    error

--- cli/qa.ts
+++ cli/qa.ts
@@ -1,13 +1,77 @@
 import YAML from 'yaml';
 import fs from 'fs';
 import path from 'path';
+import readline from 'readline';
+import shell from 'shelljs';
+import { exec } from 'child_process';
 
-if (fs.existsSync(path.resolve('.gitlab-ci.yml'))) {
-	const file = YAML.parse(fs.readFileSync(path.resolve('.gitlab-ci.yml'), 'utf8'));
+const route = path.resolve('.gitlab-ci.yml');
 
-	console.log('file', file);
+if (fs.existsSync(route)) {
+	const file = fs.readFileSync(route, 'utf8');
 
-	file.deploy_qa.script[2] = `ssh  -i ~/.ssh/key.pem $USUARIO@$IP_REMOTEQA " cd $DOCUMENT_ROOT && sudo pm2 restart backoffice "`;
+	const ymlFile = YAML.parse(file);
 
-	fs.writeFileSync(path.resolve('.gitlab-ci.yml'), YAML.stringify(file));
+	const scriptConsol = (() => {
+		switch (process.env.npm_lifecycle_event) {
+			case 'qa:files':
+				return 'files';
+
+			case 'qa:socket':
+				return 'socket';
+
+			case 'qa:providers':
+				return 'providers';
+
+			case 'qa:office':
+				return 'office';
+		}
+	})();
+
+	console.log('script', scriptConsol);
+
+	switch (scriptConsol) {
+		case 'files':
+			ymlFile.deploy_qa.script[2] =
+				'ssh  -i ~/.ssh/key.pem $USUARIO@$IP_REMOTEQA " cd $DOCUMENT_ROOT && sudo pm2 restart files "';
+
+			break;
+
+		case 'socket':
+			ymlFile.deploy_qa.script[2] =
+				'ssh  -i ~/.ssh/key.pem $USUARIO@$IP_REMOTEQA " cd $DOCUMENT_ROOT && sudo pm2 restart socket "';
+
+			break;
+
+		case 'providers':
+			ymlFile.deploy_qa.script[2] =
+				'ssh  -i ~/.ssh/key.pem $USUARIO@$IP_REMOTEQA " cd $DOCUMENT_ROOT && sudo pm2 restart providers "';
+
+			break;
+
+		case 'office':
+			ymlFile.deploy_qa.script[2] =
+				'ssh  -i ~/.ssh/key.pem $USUARIO@$IP_REMOTEQA " cd $DOCUMENT_ROOT && sudo pm2 restart backoffice "';
+			break;
+	}
+
+	const ymlString = YAML.stringify(ymlFile);
+
+	fs.writeFileSync(route, ymlString);
+
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	rl.question('commit name: ', (name) => {
+		if (shell.exec(`git add .gitlab-cli.yml`).code === 128) {
+			shell.exec(`git commit -m "${name}"`);
+			shell.exec(`git push lab-${scriptConsol}`);
+		}
+
+		shell.exit(1);
+	});
+
+	rl.on('close', () => process.exit(0));
 }
